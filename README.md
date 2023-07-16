68b1a0b44f6da3ec92d9eae44a7f9d1d6945629d
luigidenora <34819271+luigidenora@users.noreply.github.com>
2023-07-16 14:39:41 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# boilerplate
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-erltqp)
---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import {
+  Scene as SceneBase,
+  Mesh,
+  BoxGeometry,
+  MeshBasicMaterial,
+} from 'three';
+import { Main, OrthographicCamera } from '@ag-three/interaction';
+
+class Scene extends SceneBase {
+  constructor() {
+    super();
+    this.add(
+      new OrthographicCamera(5).translateZ(10),
+      new Mesh(new BoxGeometry(1), new MeshBasicMaterial({ color: 0xffff00 }))
+    );
+  }
+}
+
+new Main({ scenes: [new Scene()] } as any);
